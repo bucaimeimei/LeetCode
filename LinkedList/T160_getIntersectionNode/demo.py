b5b1e58dcac1ddc90313fe8cbd4a906870978c65
Grace <42366181+bucaimeimei@users.noreply.github.com>
2020-03-31 10:42:44 +0800

    Create demo.py

---
+++ LinkedList/T160_getIntersectionNode/demo.py
@@ -0,0 +1,12 @@
+
+
+
+class Solution(object):
+    #双指针法
+    def getIntersectionNode(self, headA, headB):
+        ha, hb = headA, headB
+        while ha != hb:
+            ha = ha.next if ha else headB
+            hb = hb.next if hb else headA
+        return ha
+
